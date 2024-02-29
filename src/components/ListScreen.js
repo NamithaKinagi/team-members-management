26d1b1c225e805684faf088ea0a282912297e452
Namitha Kinagi <nkinagi@Administrators-MacBook-Pro.local>
2024-02-29 23:53:54 +0530

    CSS changes

--- src/components/ListScreen.js
+++ src/components/ListScreen.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FaRegUserCircle, FaPlus } from "react-icons/fa";
+import "./ListScreen.css";
 
 const ListScreen = () => {
-  const members = useSelector(state => state.members.members);
-  
+  const members = useSelector((state) => state.members.members);
+
   return (
-    <div>
-      <h1>Team Members</h1>
-      <p>You have {members.length} team members</p>
-      <ul>
-        {members.map(member => (
-          <li key={member.id}>
-            {member.firstName} {member.lastName} {member.role==="admin" && '(Admin)'}
-            <Link to={`/edit/${member.id}`}>Edit</Link>
-          </li>
-        ))}
-      </ul>
-      <Link to="/add">Add Member</Link>
+    <div className="list-container">
+      <div className="header">
+        <h1>Team Members</h1>
+        <p>You have ({members.length}) members</p>
+      </div>
+      <Link to="/add" className="add-button">
+        <FaPlus />
+      </Link>
+      {members.map((member) => (
+        <div key={member.id} className="list-item">
+          <Link to={`/edit/${member.id}`} className="member-link">
+            <div className="member-details">
+              <div className="profile-icon">
+                <FaRegUserCircle />
+              </div>
+              <div className="details">
+                <p>
+                  <strong>
+                    {member.firstName} {member.lastName}{" "}
+                    {member.role==="admin" && "(Admin)"}
+                  </strong>
+                </p>
+                <p>{member.phone}</p>
+                <p>{member.email}</p>
+              </div>
+            </div>
+          </Link>
+          <hr className="separator" />
+        </div>
+      ))}
     </div>
   );
 };
